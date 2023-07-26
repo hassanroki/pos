b0b76d96628972ce43700512680e8c0e1c6cbea6
unknown <mdrokibulhassan786@gmail.com>
2023-07-26 15:00:34 +0600

    Database Create Migration

--- database/migrations/2023_07_26_060748_create_purchase_items_table.php
+++ database/migrations/2023_07_26_060748_create_purchase_items_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('purchase_items', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('product_id');
+            $table->foreignId('purchase_invoice_id');
+            $table->double('quantity');
+            $table->double('price');
+            $table->double('total');
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('purchase_items');
     }
 };
